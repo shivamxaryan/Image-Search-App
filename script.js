cde52efa6206cdb8a2864e98283b353acb09abd3
shivamxaryan <shivamaryan64@gmail.com>
2023-06-22 10:30:11 +0530

    setting up Js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "ix04iwI9RJcK836P5gOtxr0UAziSMhOSeZ5gcTMypcw"
+
+const formData=document.querySelector("form")
+const inputData=document.getElementById("search-input")
+const searchResults=document.getElementById(".search-results")
+const showMore=document.getElementById("show-more-button")
